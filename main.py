725653af875d0661d1df3fec030fdc5e6c3a1b53
radio-satellites <114111180+radio-satellites@users.noreply.github.com>
2023-03-09 10:21:51 -0500

    more thoughts

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
 import os
 from PIL import Image
+from bitstring import BitArray
 f = open("goesn_sd.frm",'rb')
 f_out = open("goesn_output.bin",'wb')
 
 bytes_in_file = os.path.getsize('goesn_sd.frm')
 ints_in_file = []
+bits_list = []
 #print(ints_in_file)
 
 ch1 = Image.new('RGB',(20000,2000))
 
 counter = 0
 
+def read_bits():
+    global bits_list #Allows for modification inside function
+    #attempt to read 80 bits/10bytes because it is convinient
+    bytes10 = f.read(10).hex() #10 bytes = 80 bits
+    #print(bytes10)
+    c = BitArray(hex=bytes10)
+    bits_string80 = c.bin[2:] #I'm not sorry about the variable names. Sorry!
+    for i in range(8): #8 because we are repacking into 10 bit frames
+        #bits_list.append(bits_string80[i*10:(i+1)*10])
+        ints_in_file.append(int(bits_string80[i*10:(i+1)*10],2))
+
+    return 0
+        
+    
+    
+    
+
 for i in range(2000000):
-    current_byte = f.read(1) #read exactly one byte
-    ints_in_file.append(int.from_bytes(current_byte, "big"))
+    #ints_in_file.append(int.from_bytes(current_byte, "big"))
     #f_out.write(int.from_bytes(current_byte, "big"))
+    read_bits()
     if counter == 1000000:
         print("Done "+str(round((i/bytes_in_file)*100))+"%!")
         counter = 0
